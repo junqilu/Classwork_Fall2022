0b23718143355267d82bdebe4d91b1418e1f9a2a
Louie Lu <jqilu@ucdavis.edu>
2022-09-27 16:07:02 -0400

    Convert patient record to dictionary that will be stored in database list

--- database.py
+++ database.py
@@ -1,53 +1,57 @@
-def create_patient_entry(patient_name, patient_id, patient_age): #Make new entries into a database list by appending to that db list
-    new_patient = [patient_name, patient_id, patient_age, []]
-    
+def create_patient_entry(patient_first_name,
+                         patient_last_name, patient_id,
+                         patient_age):
+    new_patient = {"First Name": patient_first_name,
+                   "Last Name": patient_last_name,
+                   "Id": patient_id,
+                   "Age": patient_age,
+                   "Tests": []}
     return new_patient
 
-def db_printer(inputDatabase): #Print out the whole inputDatabase
-    for line in inputDatabase: 
-        print(line)
-        print('Patient name: {}, Patient ID: {}, Patient age:{}'.format(line[0], line[1], line[2]))
-        
-    return 0
 
-def db_searcher(inputDatabase, patientID): #Search through inputDatabase for a line with matched patientID
-    for line in inputDatabase:
-        if line[1] == patientID: 
-            return line
-    
-    return False #This means nothing found for that patientID
+def print_database(db):
+    for patient in db:
+        print(patient)
+        print("Name: {}, id: {}, age: {}".format(get_full_name(patient),
+                                                 patient["Id"],
+                                                 patient["Age"]))
 
-def db_result_adder(inputDatabase, patientID, testName, testValue): #Add test name and test result into a patient's info through patientID in a inputDatabase
-    line = db_searcher(inputDatabase, patientID)
-    
-    line[3].append((testName, testValue)) #Prof wants to add testName, testValue as a tuple into that list
-    
-    return 
+
+def get_full_name(patient):
+    full_name = "{} {}".format(patient["First Name"], patient["Last Name"])
+    return full_name
+
+
+def find_patient(db, id_no):
+    for patient in db:
+        if patient["Id"] == id_no:
+            return patient
+    return False
+
+
+def add_test_to_patient(db, id_no, test_name, test_value):
+    patient = find_patient(db, id_no)
+    patient["Tests"].append((test_name, test_value))
+
+
+def adult_or_minor(patient):
+    if patient["Age"] >= 18:
+        return "adult"
+    else:
+        return "minor"
+
 
 def main():
-    db = [] #This stores all the patient lists
-    db.append(create_patient_entry("Ann Ables", 1, 30))
-    db.append(create_patient_entry("Bob Boyles", 2, 24))
-    db.append(create_patient_entry("Chris Chou", 3, 25))
-    db_printer(db)
-    
-    x = db_searcher(db, 2)
-    print(x)
-    
-    db_result_adder(db, 1, 'HDL', '100')
-    print(db)
-    
-    
-    roomList = ['Room 1', 'Room 2', 'Room 3']
-    for idx, patient in enumerate(db): 
-        print('Name = {}, Room = {}'.format(patient, roomList[idx]))
-        
-        
-    for patient, room in zip(db, roomList):
-        print('Name = {}, Room = {}'.format(patient, room))
-    
-    return 0
+    db = []
+    db.append(create_patient_entry("Ann", "Ables", 11, 30))
+    db.append(create_patient_entry("Bob", "Boyles", 22, 34))
+    db.append(create_patient_entry("Chris", "Chou", 3, 25))
+    print_database(db)
+    add_test_to_patient(db, 3, "HDL", 100)
+    print_database(db)
+    print("Patient {} is a {}".format(get_full_name(db[2]),
+                                      adult_or_minor(db[2])))
 
-    
+
 if __name__ == "__main__":
     main()
