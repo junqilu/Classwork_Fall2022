d884df6e9fe8a8a0ce607178127dff53aa74abec
Louie Lu <jqilu@ucdavis.edu>
2022-09-27 16:11:41 -0400

    Converted database from a list of patients to a dictionary of patients

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 def create_patient_entry(patient_first_name,
                          patient_last_name, patient_id,
                          patient_age):
+
     new_patient = {"First Name": patient_first_name,
                    "Last Name": patient_last_name,
                    "Id": patient_id,
@@ -10,8 +11,18 @@ def create_patient_entry(patient_first_name,
 
 
 def print_database(db):
-    for patient in db:
-        print(patient)
+    # Method one that iterates over the keys in the dictionary "db"
+    print("print_database Method #1")
+    for patient_key in db:
+        print(patient_key)
+        print("Name: {}, id: {}, age: {}"
+              .format(get_full_name(db[patient_key]),
+                      db[patient_key]["Id"],
+                      db[patient_key]["Age"]))
+
+    # Method two that iterates over the specific values in the dictionary "db"
+    print("print_database Method #2")
+    for patient in db.values():
         print("Name: {}, id: {}, age: {}".format(get_full_name(patient),
                                                  patient["Id"],
                                                  patient["Age"]))
@@ -23,10 +34,8 @@ def get_full_name(patient):
 
 
 def find_patient(db, id_no):
-    for patient in db:
-        if patient["Id"] == id_no:
-            return patient
-    return False
+    patient = db[id_no]
+    return patient
 
 
 def add_test_to_patient(db, id_no, test_name, test_value):
@@ -42,15 +51,17 @@ def adult_or_minor(patient):
 
 
 def main():
-    db = []
-    db.append(create_patient_entry("Ann", "Ables", 11, 30))
-    db.append(create_patient_entry("Bob", "Boyles", 22, 34))
-    db.append(create_patient_entry("Chris", "Chou", 3, 25))
+    # database will be a dictionary where the keys are the patient_ids
+    #   and the values will dictionaries containing patient info
+    db = {}
+    db[11] = create_patient_entry("Ann", "Ables", 11, 30)
+    db[22] = create_patient_entry("Bob", "Boyles", 22, 34)
+    db[3] = create_patient_entry("Chris", "Chou", 3, 25)
     print_database(db)
     add_test_to_patient(db, 3, "HDL", 100)
-    print_database(db)
-    print("Patient {} is a {}".format(get_full_name(db[2]),
-                                      adult_or_minor(db[2])))
+    print(db[3]["Tests"])
+    print("Patient {} is a {}".format(get_full_name(db[3]),
+                                      adult_or_minor(db[3])))
 
 
 if __name__ == "__main__":
