bf916bbab7e303cc23d29d57b53742976c8dee89
Louie Lu <jqilu@ucdavis.edu>
2022-10-02 14:44:32 -0400

    Test try/except block

---
+++ error.py
@@ -0,0 +1,15 @@
+def calc_sqaure_root(n):
+    try: 
+        from my_calculator import sqrt
+    except ModuleNotFoundError: 
+        print('The my_calculator module was not found. Loading Python math library instead.')
+        from math import sqrt 
+        
+    answer = sqrt(n)
+    return answer
+
+def main():
+    print(calc_sqaure_root(2))
+    
+if __name__ == "__main__":
+    main()
