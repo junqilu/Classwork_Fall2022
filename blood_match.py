3201dffa05a0806c2550c3bee88c4adce3198921
Louie Lu <jqilu@ucdavis.edu>
2022-10-20 11:33:42 -0400

    Added blood_match.py to track

--- blood_match.py
+++ blood_match.py
@@ -21,8 +21,7 @@ def main():
                              '/{}'.format(id))
 
             if r.status_code == 200:
-                blood_type_dict[person_type] = {'id': id, 'blood_type':
-                    r.text}
+                blood_type_dict[person_type] = {'id': id, 'blood_type': r.text}
 
         print(blood_type_dict)
 
@@ -30,14 +29,14 @@ def main():
         Judge whether it's a match for blood_type between donor and recipient
         """
         blood_match = ''
-        if blood_type_dict['Donor']['blood_type'] == blood_type_dict[
-            'Recipient']['blood_type']:
+        if blood_type_dict['Donor']['blood_type'] == \
+                blood_type_dict['Recipient']['blood_type']:
             blood_match = 'Yes'
         else:
             blood_match = 'No'
 
         """
-        Send out a POST request to see whether your judgement on the 
+        Send out a POST request to see whether your judgement on the
         blood_type match is Correct or Incorrect
         """
         post_dict = {'Name': 'jl922', 'Match': blood_match}
