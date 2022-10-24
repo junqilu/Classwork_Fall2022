5b2950569afaa49e305d76e9610fc8e4b84c5841
Louie Lu <jqilu@ucdavis.edu>
2022-10-24 12:20:52 -0400

    Updated with variable url

--- server.py
+++ server.py
@@ -48,6 +48,11 @@ def sum_numbers_from_internet():
     answer = in_data['a'] + in_data['b']
     return jsonify(answer)
 
+@app.route('/add/<a>/<b>', methods=['GET'])
+def add_variable_url(a, b):
+    answer = int(a) + int(b)
+    return jsonify(answer)
+
 
 if __name__ == '__main__':
     app.run()
